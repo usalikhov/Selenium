63dc0a9980e63803d4b1cf10c0d855e6fde1c7f0
usalikhov <usalikhov@gmail.com>
2016-03-26 15:49:39 -0400

    Update :1234:
    sdfsdf

--- src/selenium/Selenium.java
+++ src/selenium/Selenium.java
@@ -1,9 +1,36 @@
 package selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Selenium {
 
     public static void main(String[] args) {
-        System.out.println("Hi bro");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://google.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath(".//*[@id='sb_ifc0']")).sendKeys("Java tutorial");
+        driver.findElement(By.xpath(".//*[@id='sblsbb']")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            ex.getMessage();
+        }
+        driver.findElement(By.xpath(".//*[@id='hdtb-msb']/div[2]/a")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            ex.getMessage();
+        }
+        driver.findElement(By.xpath(".//*[@id='rso']/div/div[1]/div/h3/a")).click();
+        
+        System.exit(0);
+        
+        System.out.println("sadfasdf");
+
     }
 
 }
